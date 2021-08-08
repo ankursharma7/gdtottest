191284b8e5aacc89e30bde9d523782c5e40c1b8a
MD Shimul <mdshemul480@gmail.com>
2021-08-08 19:04:00 +0600

    downloader done

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ from webAutomation import Gdtot_Handler
 
 def downloader(gdtot_movie_id):
     gdtot = Gdtot_Handler()
+    g_drive = Drive("14oDKaoukF3AiIZ1g9-9fw33E5MgwPI-k")
+    folder_location = r"D:\test"
+
     # this will check the download button and return the file name
     movie_file_name = gdtot.click_on_download_get_file_name(gdtot_movie_id)
+
+    # this will search by name in gdrive and get g drive movie id
+    g_drive_movie = g_drive.get_file_id_by_name(movie_file_name)
+
+    movie_id = gdtot_movie_id["id"]
+    movie_name = g_drive_movie["name"]
+
+    # this will start downlaoding file to local storage
+    g_drive.download_by_id(movie_id, movie_name, folder_location)
+
+    # after download done this will delete the file from google drive
+    g_drive.delete_file_by_id(movie_id)
 
 
 def main():
