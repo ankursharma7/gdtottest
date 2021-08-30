0f9f8d474dddf54b8c72566c2a5263acaf3f519e
MD Shimul <mdshemul480@gmail.com>
2021-08-30 22:18:28 +0600

    removing useless import

--- webAutomation.py
+++ webAutomation.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
 
 
 class Gdtot_Handler:
